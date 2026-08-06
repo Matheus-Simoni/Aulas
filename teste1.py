3b71768e4a30f37e373a5715275fae1c11872687
EA - Matheus Henrique Maniá Simoni <matheus_simoni@acad.unasp.edu.br>
2026-08-06 19:48:56 -0300

    primeiro commit

---
+++ teste1.py
@@ -0,0 +1,27 @@
+i=0
+while i < 10:
+    print("eu gosto de banana")
+    i += 1
+
+x = 3
+y = 4
+
+resultado = (x + y)
+print(resultado)
+
+for k in range(10):
+    print("i like banana")
+
+a= [1, 2, 3] #lista
+a.append(4)
+print(a)
+
+b= (1, 2, 3) #tupla
+print(b)
+
+c= { #dicionario
+    "matheus "
+    "19 anos "
+    "estudante"
+}
+print(c)
