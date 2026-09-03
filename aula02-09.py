008a9ec5638f3f33ee06931ef8222411f44f2f70
Matheus Simoni <msimoni20072.0@gmail.com>
2026-09-03 17:46:16 -0300

    Pesquisa Binária

--- aula02-09.py
+++ aula02-09.py
@@ -1,6 +1,19 @@
 def pesquisa_sequencial(lista, item):
     for i, j in enumerate(lista):
-        if j == item
+        if j == item:
           return i
 
+def pesquisa_binaria(lista, item):
+  baixo = 0
+  alto = len(lista) - 1
 
+  while baixo <= alto:
+    meio = (baixo + alto) // 2
+    chute = lista[meio]
+    if chute == item:
+      return meio
+    elif chute > item:
+      alto = meio - 1
+    else:
+      baixo = meio + 1
+  return None
