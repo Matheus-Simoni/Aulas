f21fc566dbe6dd323e11b7e712cd6cd8fa6fbcc0
EA - Matheus Henrique Maniá Simoni <matheus_simoni@acad.unasp.edu.br>
2026-09-17 20:41:10 -0300

    Datetime

--- aula17-09.py
+++ aula17-09.py
@@ -1,10 +1,36 @@
+#importação de API + uso de datetime para manipular datas
 import requests
+import datetime
 
 def cotar(data):
-    url = f"https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?%40dataCotacao='{data}'&%24format=json" 
+    url = fr"https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao='{data}'&$top=100&$format=json&$select=cotacaoCompra"
+
     res = requests.get(url)
     res = res.json()
-    return res['value'][0]['cotacaoCompra']
 
-resultado = cotar('09-17-2026')
-print(f'Cotação do dia é R${resultado:.2f}')
+    if res['value']:
+        return res['value'][0]['cotacaoCompra']
+
+    else:
+        dia_anterior = datetime.datetime.strptime(
+            data, "%m-%d-%Y"
+        ) - datetime.timedelta(1)
+
+        dia_anterior = datetime.datetime.strftime(
+            dia_anterior, "%m-%d-%Y"
+        )
+
+        return cotar(dia_anterior)
+
+
+resultado = [
+    cotar(i) for i in [
+        "09-02-2024",
+        "09-01-2024",
+        "08-31-2024",
+        "08-30-2024",
+        "08-29-2024"
+    ]
+]
+
+print(resultado)
